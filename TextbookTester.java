c0f8d2cacab4b6bf5e9b3344ecaed2601d21c30e
mcelestino1 <111070100+mcelestino1@users.noreply.github.com>
2022-09-14 00:26:53 -0400

    Update TextbookTester.java

--- TextbookTester.java
+++ TextbookTester.java
@@ -6,11 +6,17 @@ public class TextbookTester {
         Textbook trial2 = new Textbook("Poppy goes to town", "King Daniel", 142, 2006);
 
         System.out.println(trial1);
-        System.out.println(trial2);
+        System.out.println(); // Adds new line
 
-        System.out.println();
+        System.out.println(trial2);
+        System.out.println(); // Adds new line
 
         trial1.setAuthor("Pete Muki");
         System.out.println(trial1);
+
+        System.out.println(); // Adds New Line
+        System.out.println("The author of '" + trial2.getTitle() + "' is " + trial2.getAuthor());
+
+        trial2.message();
     }
 }
